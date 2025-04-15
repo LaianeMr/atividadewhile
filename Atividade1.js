dd5c26d2d644c6120e41bfd0fea3401ae7f1a206
LaianeMr <laianerocha83@gmail.com>
2025-04-15 11:24:41 -0300

    Add files via upload

---
+++ Atividade1.js
@@ -0,0 +1,14 @@
+let hora = 12 
+ 
+if(horas >5 &&  horas <=11){
+    console.log("horas é: ", horas, "Am")
+
+else if (horas >=12 &&  horas <23)
+else if(horas >0 && horas <4){
+    console.log(" horasé", horas, "Madrugada");
+
+else }
+    console.log (" HORAS NÃO ENCONTRADA");
+}
+
+
